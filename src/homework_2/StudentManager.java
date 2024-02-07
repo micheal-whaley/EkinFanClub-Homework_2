922053aa208150bfc1c7f606525c492dbbdee9f2
Micheal Whaley <michealwhaley42@gmail.com>
2024-02-06 21:21:48 -0600

    Added functions to StudentManager

--- src/homework_2/StudentManager.java
+++ src/homework_2/StudentManager.java
@@ -1,7 +1,20 @@
 package homework_2;
 
 public class StudentManager {
-public void studentManagerThing() {
+	public void studentManagerThing() {
+			
+		}
+	public boolean readFromFile(String fileName) {
+		return true;
+	}
+	public void displayStudents() {
 		
 	}
+	public boolean searchStudentById(int id) {
+		return true;
+	}
+	public boolean updateStudentGradeById(int id, double grade) {
+		return true;
+	}
+	
 }
