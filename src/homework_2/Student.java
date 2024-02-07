8e259d539b90905b43753453ceae834bc30cfbc4
Micheal Whaley <michealwhaley42@gmail.com>
2024-02-07 11:48:54 -0600

    Added all methods to Student Class

--- src/homework_2/Student.java
+++ src/homework_2/Student.java
@@ -4,4 +4,40 @@ public class Student {
 	public void studentThing() {
 		
 	}
+	public Student(int newId, String newName, double newGrade) {
+		id = newId;
+		Name = newName;
+		grade = newGrade;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return Name;
+	}
+
+	public void setName(String name) {
+		Name = name;
+	}
+
+	public double getGrade() {
+		return grade;
+	}
+
+	public void setGrade(double grade) {
+		this.grade = grade;
+	}
+	
+	public String toString() {
+		return ("Id: "+id+" Name: "+Name+" Grade: "+grade);
+	}
+
+	private int id;
+	private String Name;
+	private double grade;
 }
