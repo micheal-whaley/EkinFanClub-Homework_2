af0fb26039ae27f3d126183302e8f5c696ae0aea
Van Grabner <grabner737@gmail.com>
2024-02-09 16:23:34 -0600

    prepare for pull

--- src/homework_2/StudentManager.java
+++ src/homework_2/StudentManager.java
@@ -1,34 +1,31 @@
 package homework_2;
 
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class StudentManager {
-	List<Student> students = new ArrayList<Student>();
-	public boolean readFromFile(String fileName) {
-		FileInputStream studentData;
-		try {
-			studentData = new FileInputStream("files/" + fileName);
-			Scanner sc = new Scanner(studentData);
-			while (sc.hasNext()) {
-				Student stew = new Student();
-				stew.setId(sc.nextInt());
-				stew.setName(sc.next() + " " + sc.next());
-				stew.setGrade(sc.nextDouble());
-				students.add(stew);
-				System.out.println(stew.toString());
-			}
-			studentData.close();
-			sc.close();
-			return true;
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-			return false;
+	public void studentManagerThing() {
+			
 		}
+	List<Student> students = new ArrayList<Student>();
+	public boolean readFromFile(String fileName) throws IOException {
+		FileInputStream studentData = new FileInputStream("files/" + fileName);
+		Scanner sc = new Scanner(studentData);
+		while (sc.hasNext()) {
+			Student stew = new Student();
+			stew.setId(sc.nextInt());
+			stew.setName(sc.next() + " " + sc.next());
+			stew.setGrade(sc.nextDouble());
+			students.add(stew);
+			System.out.println(stew.toString());
+		}
+		sc.close();
+        studentData.close(); 
+		return true;
 	}
 	public void displayStudents() {
 		
@@ -44,6 +41,8 @@ public class StudentManager {
 		return false;
 	}
 	public boolean updateStudentGradeById(int id, double grade) {
+		
+
 		return true;
 	}
 	
